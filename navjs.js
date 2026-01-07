041589ac16cb77e5279fe1dd929006dc658b1bc9
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2026-01-08 01:37:36 +0700

    Update navjs.js

--- navjs.js
+++ navjs.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
   $nav
     .addClass('navbar navbar-expand-lg navbar-dark fixed-top')
     .css({
-      width: "100vw",
       position: 'fixed',
       'z-index': 99999,
       transition:
