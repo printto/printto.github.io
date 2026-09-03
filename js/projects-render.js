73a76a4384e701883906df03dc209a12962114f7
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2026-09-04 01:34:20 +0700

    Update icons

--- js/projects-render.js
+++ js/projects-render.js
@@ -60,15 +60,9 @@
       vb: "66 64 512 512",
       d: "M389.6 298.3L168.9 77L449.7 238.2L389.6 298.3zM111.3 64C98.3 70.8 89.6 83.2 89.6 99.3L89.6 540.6C89.6 556.7 98.3 569.1 111.3 575.9L367.9 319.9L111.3 64zM536.5 289.6L477.6 255.5L411.9 320L477.6 384.5L537.7 350.4C555.7 336.1 555.7 303.9 536.5 289.6zM168.9 563L449.7 401.8L389.6 341.7L168.9 563z"
     },
-    store: {
+    appstore: {
       vb: "0 0 24 24",
-      rule: true,
-      d: "M12 1a5 5 0 0 0-5 5v1H3.4l1.1 14.2A2 2 0 0 0 6.5 23h11a2 2 0 0 0 2-1.8L20.6 7H17V6a5 5 0 0 0-5-5zm0 2a3 3 0 0 1 3 3v1H9V6a3 3 0 0 1 3-3z"
-    },
-    play: {
-      vb: "0 0 24 24",
-      rule: true,
-      d: "M2 4h20a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1zm8 4.2v7.6l6.5-3.8L10 8.2z"
+      d: "M22 17.607c-.786 2.28-3.139 6.317-5.563 6.361-1.608.031-2.125-.953-3.963-.953-1.837 0-2.412.923-3.932.983-2.572.099-6.542-5.827-6.542-10.995 0-4.747 3.308-7.1 6.198-7.143 1.55-.028 3.014 1.045 3.959 1.045.949 0 2.727-1.29 4.596-1.101.782.033 2.979.315 4.389 2.377-3.741 2.442-3.158 7.549.858 9.426zm-5.222-17.607c-2.826.114-5.132 3.079-4.81 5.531 2.612.203 5.118-2.725 4.81-5.531z"
     },
     download: {
       vb: "0 0 24 24",
@@ -93,13 +87,12 @@
     if (/itch\.io/.test(href) || /itch\.io/.test(label)) return "itch";
     if (/steampowered\.com/.test(href) || /steam/.test(label)) return "steam";
     if (/play\.google\.com/.test(href) || /google play/.test(label)) return "playstore";
-    if (/apps\.apple\.com/.test(href) || /appstore|app store/.test(label)) return "store";
+    if (/apps\.apple\.com/.test(href) || /appstore|app store/.test(label)) return "appstore";
     if (/drive\.google\.com/.test(href) || /apk|download/.test(label)) return "download";
     if (/youtube\.com/.test(href) || /youtube/.test(label)) return "youtube";
-    if (/play on browser|play in browser/.test(label)) return "play";
     if (/ar filter|facebook ar|spark ar/.test(label)) return "filters";
     if (/wiki|article|archive|facebook\.com/.test(href + " " + label)) return "article";
-    if (/website|deployed/.test(label)) return "globe";
+    if (/website|deployed|play on browser|play in browser/.test(label)) return "globe";
     return "link";
   }
 
