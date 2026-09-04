afab363500137f5ae2af4d1f065dd899d7f4ffe4
Pappim Pipatkasrira <printtopipatkasira@gmail.com>
2026-09-04 23:07:28 +0700

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -11,7 +11,7 @@ window.PROJECTS = [
     ],
     body: [
       "A cutting-edge platform that turns an ordinary room photo into your dream room with just one click, from SCG DIGITAL",
-      "Using only a single indoor/outdoor image."
+      "Reconstructs the room in 3D from a single indoor/outdoor image. Add 3D furniture, change floor tilings and walls."
     ],
     links: [
       { l: "SCG Digital", href: "https://www.facebook.com/SCGdigital/photos/%E0%B9%80%E0%B8%9B%E0%B8%B4%E0%B8%94%E0%B8%9B%E0%B8%A3%E0%B8%B0%E0%B8%AA%E0%B8%9A%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%93%E0%B9%8C%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88%E0%B8%81%E0%B8%B1%E0%B8%9A-weshape-%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%87%E0%B8%B2%E0%B8%99-asa-%E0%B8%A3%E0%B8%B9%E0%B9%89%E0%B8%88%E0%B8%B1%E0%B8%81%E0%B9%81%E0%B8%9E%E0%B8%A5%E0%B8%95%E0%B8%9F%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%A1%E0%B8%AA%E0%B8%B8%E0%B8%94%E0%B8%A5%E0%B9%89%E0%B8%B3-%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%8A%E0%B9%88%E0%B8%A7%E0%B8%A2%E0%B9%80%E0%B8%9B%E0%B8%A5%E0%B8%B5%E0%B9%88%E0%B8%A2%E0%B8%99%E0%B8%A0%E0%B8%B2%E0%B8%9E/1275873391206607/?_rdr" }
